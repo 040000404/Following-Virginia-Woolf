5ea1a52a24a1b617065b8977b953e0fec2314f18
melonobi <50364669+melonobi@users.noreply.github.com>
2020-03-03 13:14:16 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,13 +36,13 @@ var header = document.getElementById("header");
 function scrollFunction() {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         document.getElementById("header").style.fontSize = "0px";
-        document.getElementById("navbar").style.fontSize = "30px";
-        document.getElementById("navbar").style.backgroundColor = "#f8f8f5";
+        document.getElementById("headerlogo").style.width = "100px";
+        document.getElementById("navbar").style.backgroundImage = "transparent";
     }
     
     else {
         document.getElementById("header").style.fontSize = "90px";
-        document.getElementById("navbar").style.fontSize = "0px";
+        document.getElementById("headerlogo").style.width = "0px";
         document.getElementById("navbar").style.backgroundColor = "transparent";
 
     }
@@ -60,107 +60,13 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-//delay transition
-'use strict';
-
-function hasClass(el, className) {
-   if (el.classList)
-      return el.classList.contains(className);
-   else
-      return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-}
-
-function addClass(el, classNames) {
-   classNames = classNames.split(' ')
-   if (el.classList)
-      classNames.forEach((classname) => el.classList.add(classname))
-   else if (!hasClass(el, className))
-      classNames.forEach((classname) => el.className += " " + classname)
-}
-
-function removeClass(el, className) {
-   if (el.classList)
-      el.classList.remove(className);
-   else if (hasClass(el, className)) {
-      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-      el.className = el.className.replace(reg, ' ');
-   }
-}
-
-;(function() {
-   let lastAnimation = 0,
-      sections = Array.prototype.slice.call(document.querySelectorAll('.box')),
-      currentSection = 0,
-      len = sections.length
-
-   const handleScroll = (e) => {
-      e.preventDefault()
-
-      let timeNow = Date.now(),
-         delta = e.deltaY,
-         newSection
-
-      if (timeNow - lastAnimation < 2000) {
-         return;
-      }
-
-      if (e.deltaY < 0) {
-         // Up
-         newSection = (currentSection > 0) ? currentSection - 1 : currentSection
-      } else {
-         // Down
-         newSection = (currentSection < len - 1) ? currentSection + 1 : currentSection
-      }
-
-      if (currentSection !== newSection) {
-         // newSection is currentSection now
-         // And `old` currentSection is previousSection
-         if (currentSection < newSection) {
-            // Moving Down
-            removeClass(sections[newSection], 'under')
-            addClass(sections[newSection], 'current')
-            removeClass(sections[currentSection], 'current')
-            addClass(sections[currentSection], 'above')
-         } else {
-            // Moving Up
-            removeClass(sections[newSection], 'above')
-            addClass(sections[newSection], 'current')
-            removeClass(sections[currentSection], 'current')
-            addClass(sections[currentSection], 'under')
-         }
-
-         // Update currentSection
-         currentSection = newSection
-
-         sections.forEach((item, index) => {
-            // item.style.top = `${(index - currentSection) * 100}vh`
-            item.style.transform = `translateY(${(index - currentSection) * 100}vh)`
-         })
-      }
-
-      lastAnimation = timeNow
-   }
-
-   // first-load
-   addClass(sections[currentSection], 'current')
-   document.addEventListener('keydown', (ev) => {
-		if (ev.keyCode === 38 || ev.keyCode === 40) {
-			ev.preventDefault()
-			return false
-		}
-	}, false)
-   document.addEventListener('wheel', handleScroll)
-})()
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -169,15 +75,14 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-
